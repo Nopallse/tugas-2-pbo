76c67067ecd00f9e0629a8f1fb30b37788b776e4
Naufal <noppal.901@gmail.com>
2023-12-03 15:05:26 +0700

    oke

--- src/App.java
+++ src/App.java
@@ -1,35 +1,40 @@
 import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class App {
     public static void main(String[] args) throws Exception {
-         try {
+        try {
             Scanner scanner = new Scanner(System.in);
-
-            System.out.print("Masukkan No Faktur: ");
+            System.out.println("                   MINI MARKET");
+            System.out.println("=================================================");
+            System.out.printf("Masukkan No Faktur\t\t: ");
             String noFaktur = scanner.nextLine();
 
-            System.out.print("Masukkan Nama Pelanggan: ");
+            System.out.printf("Masukkan Nama Pelanggan\t\t: ");
             String namaPelanggan = scanner.nextLine();
 
-            System.out.print("Masukkan Nama Barang: ");
+            System.out.printf("Masukkan Nama Barang\t\t: ");
             String namaBarang = scanner.nextLine();
 
-            System.out.print("Masukkan Harga Barang: ");
+            System.out.printf("Masukkan Harga Barang\t\t: ");
             double hargaBarang = scanner.nextDouble();
 
-            System.out.print("Masukkan Jumlah Beli: ");
+            System.out.printf("Masukkan Jumlah Beli\t\t: ");
             int jumlahBeli = scanner.nextInt();
 
-            // Membuat objek BarangPembayaran
-            BarangPembayaran barangPembayaran = new BarangPembayaran(namaBarang, hargaBarang, jumlahBeli);
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+            SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm:ss a zzz");
+            Date date = new Date();
 
-            // Membuat objek Faktur
-            Faktur faktur = new Faktur(noFaktur, namaPelanggan, barangPembayaran);
+            System.out.printf("Tanggal Transaksi\t\t: %s\n", dateFormat.format(date));
+            System.out.printf("Jam\t\t\t\t: %s\n", timeFormat.format(date));
+            System.out.printf("Kasir\t\t\t\t: naufal\n");
 
-            // Menampilkan faktur
-            faktur.cetakFaktur();
+            
+
+
         } catch (Exception e) {
-            // Menangkap exception yang terjadi selama input
             System.out.println("Terjadi kesalahan saat memproses input: " + e.getMessage());
         }
     }
